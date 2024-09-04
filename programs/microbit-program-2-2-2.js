ba8058627e6b53d2db4a7af74ef853a08cc9bcd7
FeyiTijani <ftijani@msudenver.edu>
2024-09-03 21:17:33 -0400

    Create microbit-program-2-2-2.js

---
+++ programs/microbit-program-2-2-2.js
@@ -0,0 +1,4 @@
+//This program will display a series of icons
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.Asleep)
+basic.showNumber(5) // i chose this icon because its my favorite number
